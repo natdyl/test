e628aeda0fb73e554e6804f7775638d123bcac64
Dylikowska <natalia.dylikowska@accenture.com>
2017-03-07 15:52:53 +0100

    fixes2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("Tekst na probebleble");
+        System.out.println("Tekst na probebllllllleble");
     }
 
 }
